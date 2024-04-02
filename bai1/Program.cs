9ad1c79ae0b5ee9d0cabd03f98d535529341c3c2
daidh25 <dinhhuydai1234@gmail.com>
2024-04-02 10:17:13 +0700

    btvnb4

---
+++ bai1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal class Sach
+{
+    public struct Thongtinsach
+    {
+        public string Tieude { get; set; }
+        public string Tacgia { get; set; }
+        public DateTime NamXB { get; set; }
+
+        public Thongtinsach(string _tieude, string _tacgia, DateTime _namXB)
+        {
+            Tieude = _tieude;
+            Tacgia = _tacgia;
+            NamXB = _namXB;
+        }
+
+        public bool KiemTraThongTin()
+        {
+            if (string.IsNullOrEmpty(Tieude) || string.IsNullOrEmpty(Tacgia))
+            {
+                Console.WriteLine("Thông tin nhập vào không hợp lệ!");
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        List<Thongtinsach> dsSach = DanhSachthongtin();
+        QuanLyThongTinSach(dsSach);
+    }
+
+    public static List<Thongtinsach> DanhSachthongtin()
+    {
+        List<Thongtinsach> ds = new List<Thongtinsach>
+        {
+            new Thongtinsach("Java", "Nguyen Anh", new DateTime(2012, 1, 1)),
+            new Thongtinsach("Python", "Quang Huy", new DateTime(2013, 2, 2)),
+            new Thongtinsach("SQL", "Minh Anh", new DateTime(2014, 3, 3)),
+            new Thongtinsach("C#", "Van Tinh", new DateTime(2015, 4, 4))
+        };
+
+        return ds;
+    }
+
+    public static void QuanLyThongTinSach(List<Thongtinsach> ds)
+    {
+        Console.WriteLine("=================================Quản lý thông tin sách=====================");
+        Console.WriteLine("1. Thêm sách");
+        Console.WriteLine("2. Hiển thị danh sách");
+        Console.WriteLine("3. Tìm sách");
+        Console.WriteLine("4. Exit");
+
+        int luaChon;
+        do
+        {
+            Console.WriteLine("Nhập lựa chọn:");
+            luaChon = int.Parse(Console.ReadLine());
+
+            switch (luaChon)
+            {
+                case 1:
+                    ThemSach(ds);
+                    break;
+                case 2:
+                    HienThiDS(ds);
+                    break;
+                case 3:
+                    TimSach(ds);
+                    break;
+                case 4:
+                    Console.WriteLine("Exit!");
+                    break;
+                default:
+                    Console.WriteLine("Lua chon khong hop le! Vui long chon lai");
+                    break;
+            }
+        } while (luaChon != 4);
+    }
+
+    public static void ThemSach(List<Thongtinsach> ds)
+    {
+        Thongtinsach sachMoi = new Thongtinsach();
+        Console.WriteLine("Nhập thông tin sách mới: ");
+        Console.WriteLine("Nhập tiêu đề: ");
+        sachMoi.Tieude = Console.ReadLine();
+        Console.WriteLine("Nhập tác giả: ");
+        sachMoi.Tacgia = Console.ReadLine();
+        Console.WriteLine("Nhập năm xuất bản: ");
+        sachMoi.NamXB = DateTime.Parse(Console.ReadLine());
+
+        if (sachMoi.KiemTraThongTin())
+        {
+            ds.Add(sachMoi);
+            Console.WriteLine("Thông tin sách đã được thêm!");
+        }
+    }
+
+    public static void HienThiDS(List<Thongtinsach> ds)
+    {
+        Console.WriteLine("Hiển thị danh sách: ");
+        foreach (var sach in ds)
+        {
+            Console.WriteLine($"Tiêu đề: {sach.Tieude}, Tác giả: {sach.Tacgia}, Năm xuất bản: {sach.NamXB:yyyy}");
+        }
+    }
+
+    public static void TimSach(List<Thongtinsach> ds)
+    {
+        Console.Write("Nhập tiêu đề sách mà bạn cần tìm kiếm: ");
+        string search = Console.ReadLine();
+        int k = 0;
+        for (int i = 0; i < ds.Count; i++)
+        {
+            if (ds[i].Tieude.Equals(search))
+            {
+                Console.WriteLine($"Tiêu đề: {ds[i].Tieude}, Tác giả: {ds[i].Tacgia}, Năm xuất bản: {ds[i].NamXB:yyyy}");
+                k++;
+            }
+        }
+        if (k == 0)
+            Console.WriteLine("Không tìm thấy sách nào có tiêu đề trên!!!");
+    }
+
+}
---
+++ bai2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Program
+{
+    public struct ThongTinSinhVien
+    {
+        public string Ten { get; set; }
+        public int Tuoi { get; set; }
+        public double DiemTrungBinh { get; set; }
+
+        public ThongTinSinhVien(string ten, int tuoi, double diemTrungBinh)
+        {
+            Ten = ten;
+            Tuoi = tuoi;
+            DiemTrungBinh = diemTrungBinh;
+        }
+
+        public override string ToString()
+        {
+            return $"Tên: {Ten}, Tuổi: {Tuoi}, Điểm Trung Bình: {DiemTrungBinh}";
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        List<ThongTinSinhVien> danhSachSinhVien = DanhSachSinhVien();
+
+        Console.WriteLine("==== Quản Lý Thông Tin Sinh Viên ====");
+        int luaChon;
+        do
+        {
+            Console.WriteLine("\n1. Thêm sinh viên mới");
+            Console.WriteLine("2. Hiển thị danh sách sinh viên");
+            Console.WriteLine("3. Tìm kiếm sinh viên theo tên");
+            Console.WriteLine("4. Thoát chương trình");
+            Console.Write("Nhập lựa chọn của bạn: ");
+            luaChon = int.Parse(Console.ReadLine());
+
+            switch (luaChon)
+            {
+                case 1:
+                    ThemSinhVien(danhSachSinhVien);
+                    break;
+                case 2:
+                    HienThiDanhSachSinhVien(danhSachSinhVien);
+                    break;
+                case 3:
+                    TimKiemSinhVienTheoTen(danhSachSinhVien);
+                    break;
+                case 4:
+                    Console.WriteLine("Thoát chương trình...");
+                    break;
+                default:
+                    Console.WriteLine("Lựa chọn không hợp lệ! Vui lòng chọn lại.");
+                    break;
+            }
+        } while (luaChon != 4);
+    }
+
+    public static List<ThongTinSinhVien> DanhSachSinhVien()
+    {
+        List<ThongTinSinhVien> ds = new List<ThongTinSinhVien>
+        {
+            new ThongTinSinhVien("Nguyen Van An", 20, 8.5),
+            new ThongTinSinhVien("Tran Thi Binh", 21, 7.9),
+            new ThongTinSinhVien("Hoang Van Viet", 19, 9.0),
+            new ThongTinSinhVien("Le Thi Nhung", 22, 8.2)
+        };
+
+        return ds;
+    }
+
+    public static void ThemSinhVien(List<ThongTinSinhVien> danhSachSinhVien)
+    {
+        Console.WriteLine("\nThêm sinh viên mới:");
+        Console.Write("Nhập tên sinh viên: ");
+        string ten = Console.ReadLine();
+        Console.Write("Nhập tuổi sinh viên: ");
+        int tuoi = int.Parse(Console.ReadLine());
+        Console.Write("Nhập điểm trung bình: ");
+        double diemTrungBinh = double.Parse(Console.ReadLine());
+
+        ThongTinSinhVien sinhVienMoi = new ThongTinSinhVien(ten, tuoi, diemTrungBinh);
+        danhSachSinhVien.Add(sinhVienMoi);
+
+        Console.WriteLine("Thêm sinh viên thành công!");
+    }
+
+    public static void HienThiDanhSachSinhVien(List<ThongTinSinhVien> danhSachSinhVien)
+    {
+        Console.WriteLine("\nDanh sách sinh viên:");
+        foreach (ThongTinSinhVien sinhVien in danhSachSinhVien)
+        {
+            Console.WriteLine(sinhVien);
+        }
+    }
+
+    public static void TimKiemSinhVienTheoTen(List<ThongTinSinhVien> danhSachSinhVien)
+    {
+        Console.Write("\nNhập tên sinh viên cần tìm kiếm: ");
+        string tenCanTim = Console.ReadLine();
+
+        bool timThay = false;
+        foreach (ThongTinSinhVien sinhVien in danhSachSinhVien)
+        {
+            if (sinhVien.Ten.Equals(tenCanTim))
+            {
+                Console.WriteLine("Thông tin sinh viên cần tìm:");
+                Console.WriteLine(sinhVien);
+                timThay = true;
+                break;
+            }
+        }
+
+        if (!timThay)
+        {
+            Console.WriteLine("Không tìm thấy sinh viên có tên này.");
+        }
+    }
+}
